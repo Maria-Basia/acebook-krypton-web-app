1074c8bcc7b6691a0f0ac2c1127f50d3b82b8871
Jason <jason.sayeed@gmail.com>
2024-04-15 09:57:35 +0100

    created individual post and comment components

--- frontend/src/components/Comment/Comments.jsx
+++ frontend/src/components/Comment/Comments.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getComments } from "../../services/comments";
-import { LikeCommentButton } from "../Like/Like";
+import Comment from "./Comment";
 import CreateComment from "./CreateComment";
-import "./Comments.css";
 
 const Comments = ({ postId }) => {
-  const [commentData, setCommentData] = useState([]);
+  const [comments, setComments] = useState([]);
 
   const loggedInUser = localStorage.getItem("userId");
 
@@ -15,7 +14,7 @@ const Comments = ({ postId }) => {
       getComments(postId, token) // Fetch comments for the specific post
         .then((data) => {
           console.log("Received comment data:", data);
-          setCommentData(data.comments);
+          setComments(data.comments);
         })
         .catch((error) => console.error("Error fetching comments:", error));
     }
@@ -25,62 +24,17 @@ const Comments = ({ postId }) => {
     fetchComments();
   }, []);
 
-  const addOrdinalSuffix = (day) => {
-    if (day === 1 || day === 21 || day === 31) return day + "st";
-    if (day === 2 || day === 22) return day + "nd";
-    if (day === 3 || day === 23) return day + "rd";
-    return day + "th";
-  };
-
   return (
-    <div>
+    <>
       <div className="create-comment">
         <CreateComment postId={postId} updateCommentFeed={fetchComments} />
       </div>
-      <div>
-        {commentData.map((comment) => {
-          const date = new Date(comment.createdAt);
-          const day = addOrdinalSuffix(date.getDate());
-          const month = date.toLocaleString("en-GB", { month: "short" });
-          const time = date.toLocaleString("en-GB", {
-            hour: "numeric",
-            minute: "numeric",
-          });
-
-          const formattedDate = `${day} ${month} ${date.getFullYear()} at ${time}`;
-
-          return (
-            <div className="comment-container" key={comment._id}>
-              <div className="comment-content">
-                <div className="comment-user">
-                  <img
-                    className="comment-profilePicture"
-                    src={comment.user.profilePicture}
-                  />
-                  <div className="comment-date-and-name">
-                    <div className="comment-fullName">
-                      {comment.user.fullName}
-                    </div>
-                    <div className="comment-date">{formattedDate}</div>
-                  </div>
-                </div>
-                <div className="comment-text">{comment.comment_text}</div>
-                <div className="comment-like-button">
-                  <LikeCommentButton
-                    commentId={comment._id}
-                    isLiked={comment.likedBy.includes(loggedInUser)}
-                    updateCommentLikeFeed={fetchComments}
-                  />
-                </div>
-                <div className="comment-like-count">
-                  {comment.likedBy.length}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      <Comment
+        comments={comments}
+        loggedInUser={loggedInUser}
+        fetchComments={fetchComments}
+      />
+    </>
   );
 };
 
--- frontend/src/components/Post/Posts.jsx
+++ frontend/src/components/Post/Posts.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { getPosts } from "../../services/posts";
-import { LikePostButton } from "../Like/Like";
 import CreatePost from "./CreatePost";
+import Post from "./Post";
 import { useNavigate } from "react-router-dom";
-import Comments from "../Comment/Comments";
-import "./Posts.css";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -32,67 +30,20 @@ const Posts = () => {
     fetchPosts();
   }, []);
 
-  const addOrdinalSuffix = (day) => {
-    if (day === 1 || day === 21 || day === 31) return day + "st";
-    if (day === 2 || day === 22) return day + "nd";
-    if (day === 3 || day === 23) return day + "rd";
-    return day + "th";
-  };
-
   return (
     <>
       <div className="createpost">
         <CreatePost updatePostFeed={fetchPosts} />
       </div>
       <div>
-        {posts.map((post) => {
-          const date = new Date(post.createdAt);
-          const day = addOrdinalSuffix(date.getDate());
-          const month = date.toLocaleString("en-GB", { month: "short" });
-          const time = date.toLocaleString("en-GB", {
-            hour: "numeric",
-            minute: "numeric",
-          });
-
-          const formattedDate = `${day} ${month} ${date.getFullYear()} at ${time}`;
-
-          return (
-            <div className="post" key={post._id}>
-              <div className="post-border">
-                <div className="post-header-container">
-                  <img
-                    className="post-profile_picture"
-                    src={post.user.profilePicture}
-                    alt="Profile"
-                  />
-                  <div>
-                    <div className="post-user-fullName">
-                      {post.user.fullName}
-                    </div>
-                    <div className="post-date">{formattedDate}</div>
-                  </div>
-                </div>
-                {post.image && (
-                  <img className="post_image" src={post.image} alt="Post" />
-                )}
-                <div className="post-message">{post.message}</div>
-                <div className="post-like-button">
-                  <LikePostButton
-                    postId={post._id}
-                    isLiked={post.likedBy.includes(loggedInUser)}
-                    updatePostLikeFeed={fetchPosts}
-                  />
-                </div>
-                <div className="post-like-counter">{post.likedBy.length}</div>
-              </div>
-              <div>
-                <Comments postId={post._id} />
-              </div>
-            </div>
-          );
-        })}
+        <Post
+          posts={posts}
+          loggedInUser={loggedInUser}
+          fetchPosts={fetchPosts}
+        />
       </div>
     </>
   );
 };
+
 export default Posts;
